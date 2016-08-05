7730a8f1fb20e384cc600274348751902ca3f394
sebseb7 <github@seb.exse.net>
2016-08-05 03:56:08 +0200

    no more sqrt for circles, major speedup on embedded

--- circle.c
+++ circle.c
@@ -1,11 +1,7 @@
-#include "stdio.h"
 #include <math.h>
 
 #include "circle.h"
 #include "rect.h"
-
-#include "armmath.h"
-
 
 static void dla_plot(int x, int y, uint8_t r,uint8_t g , uint8_t b, float br)
 {
@@ -96,17 +92,21 @@ void draw_circle(
 		uint8_t g,
 		uint8_t b )
 {
+	
+	int maxins = rad*rad;
+	int maxbor = (rad-t)*(rad-t);
+	int maxbor2 = (rad-(t+1))*(rad-(t+1));
+	int maxcir = (rad-1)*(rad-1);
 
-	float i,j;
-
-	t++;
-
-	for(i=0;i<rad;i++)
+	for(int i=rad;i>=0;i--)
 	{
-		for(j=0;j<(i+1);j++)
+		int dist_a = i*i;
+
+		for(int j=0;j<(i+1);j++)
 		{
-			float dist = pythagorasf( j,i );
-			if((dist <= rad-1)&&(dist >= ((rad-t)+1)))
+			int dist = dist_a + j*j;
+			
+			if((dist <= maxcir)&&(dist >= maxbor))
 			{
 				setLedXY(x-j,y+i,r,g,b);
 				setLedXY(x+j,y+i,r,g,b);
@@ -119,29 +119,35 @@ void draw_circle(
 				setLedXY(x+i,y-j,r,g,b);
 
 			}
-			else if((dist < rad)&&(dist > rad-1))
+			else if((dist < maxins)&&(dist > maxcir))
 			{
-				dla_plot(x-j,y+i,r,g,b,1-(dist-rad+1));
-				dla_plot(x+j,y+i,r,g,b,1-(dist-rad+1));
-				dla_plot(x+j,y-i,r,g,b,1-(dist-rad+1));
-				dla_plot(x-j,y-i,r,g,b,1-(dist-rad+1));
+				float pct = 1 - ((dist-maxcir)/(float)(maxins-maxcir));
 
-				dla_plot(x-i,y+j,r,g,b,1-(dist-rad+1));
-				dla_plot(x+i,y+j,r,g,b,1-(dist-rad+1));
-				dla_plot(x+i,y-j,r,g,b,1-(dist-rad+1));
-				dla_plot(x-i,y-j,r,g,b,1-(dist-rad+1));
+				dla_plot(x-j,y+i,r,g,b,pct);
+				dla_plot(x+j,y+i,r,g,b,pct);
+				dla_plot(x+j,y-i,r,g,b,pct);
+				dla_plot(x-j,y-i,r,g,b,pct);
+
+				dla_plot(x-i,y+j,r,g,b,pct);
+				dla_plot(x+i,y+j,r,g,b,pct);
+				dla_plot(x+i,y-j,r,g,b,pct);
+				dla_plot(x-i,y-j,r,g,b,pct);
+
 			}
-			else if((dist < (rad-t+1))&&(dist > rad-t))
+			else if((dist < maxbor)&&(dist >= maxbor2))
 			{
-				dla_plot(x-j,y+i,r,g,b,dist-(rad-t));
-				dla_plot(x+j,y+i,r,g,b,dist-(rad-t));
-				dla_plot(x+j,y-i,r,g,b,dist-(rad-t));
-				dla_plot(x-j,y-i,r,g,b,dist-(rad-t));
+				float pct = 1 - ((dist-maxbor)/(float)(maxbor2-maxbor));
 
-				dla_plot(x-i,y+j,r,g,b,dist-(rad-t));
-				dla_plot(x+i,y+j,r,g,b,dist-(rad-t));
-				dla_plot(x+i,y-j,r,g,b,dist-(rad-t));
-				dla_plot(x-i,y-j,r,g,b,dist-(rad-t));
+				dla_plot(x-j,y+i,r,g,b,pct);
+				dla_plot(x+j,y+i,r,g,b,pct);
+				dla_plot(x+j,y-i,r,g,b,pct);
+				dla_plot(x-j,y-i,r,g,b,pct);
+
+				dla_plot(x-i,y+j,r,g,b,pct);
+				dla_plot(x+i,y+j,r,g,b,pct);
+				dla_plot(x+i,y-j,r,g,b,pct);
+				dla_plot(x-i,y-j,r,g,b,pct);
+
 			}
 		}
 	}
@@ -169,24 +175,24 @@ void draw_filledEllipse(
 	float x,y,d;
 	x = 0;
 	y = b;
-	d = pow(b,2) - pow(a,2)*b + pow(a,2)/4;
+	d = powf(b,2) - powf(a,2)*b + powf(a,2)/4;
 	onEllipsePoint(xcenter,ycenter,x,y,r,g,b2);
 	while( (a*a*(y-.5)) > (b*b*(x+1)) )
 	{
 		if(d<0)
 		{
-			d = d + pow(b,2)*(2*x+3);
+			d = d + powf(b,2)*(2*x+3);
 			x=x+1;
 		}
 		else
 		{
-			d = d + pow(b,2)*(2*x+3) + pow(a,2)*(-2*y+2);
+			d = d + powf(b,2)*(2*x+3) + powf(a,2)*(-2*y+2);
 			x=x+1;
 			y=y-1;
 		}
 		onEllipsePoint(xcenter,ycenter,x,y,r,g,b2);
 	}
-	d = b*b*pow((x+.5),2) + a*a*pow((y-1),2) - a*a*b*b;
+	d = b*b*powf((x+.5),2) + a*a*powf((y-1),2) - a*a*b*b;
 	while(y>0)
 	{
 		if(d<0)
